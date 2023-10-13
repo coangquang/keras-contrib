8bd62f72c8673a4ef639d266554db088e24f9ba9
Nhật Quang <83051297+coangquang@users.noreply.github.com>
2023-10-13 13:16:38 +0700

    Update crf.py

--- keras_contrib/layers/crf.py
+++ keras_contrib/layers/crf.py
@@ -15,6 +15,7 @@ from keras_contrib.losses import crf_loss
 from keras_contrib.metrics import crf_marginal_accuracy
 from keras_contrib.metrics import crf_viterbi_accuracy
 from keras_contrib.utils.test_utils import to_tuple
+import tensorflow as tf
 
 
 class CRF(Layer):
@@ -460,7 +461,7 @@ class CRF(Layer):
             if K.backend() == 'theano':
                 m = states[3][:, t:(t + 2)]
             else:
-                m = K.slice(states[3], [0, t], [-1, 2])
+                m = tf.slice(states[3], [0, t], [-1, 2])
             input_energy_t = input_energy_t * K.expand_dims(m[:, 0])
             # (1, F, F)*(B, 1, 1) -> (B, F, F)
             chain_energy = chain_energy * K.expand_dims(
